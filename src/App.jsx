92ef55415d47426d29da1c4f21e3896015bd3793
Saikat-D023 <saikatdey358@gmail.com>
2024-12-06 15:53:21 +0530

    useFetch was added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import {usePostTitle} from './hooks/useFetch'
+import {useFetch} from './hooks/useFetch'
 
-function App() {
-  const postTitle = usePostTitle();
+// function App() {
+//   const postTitle = usePostTitle();
+//   return (
+//     <div>
+//       {postTitle}
+//     </div>
+//   )
+// }
+
+function App(){
+  const [currentPost , setCurrentPost] = useState(1);
+  const { finalData , loading} = useFetch("https://jsonplaceholder.typicode.com/posts/" + currentPost);
+
+  //assignment: hit the button every 10 secs
+
+  if( loading ){
+    return (<div>
+      Loading...
+    </div>)
+  }
+
   return (
     <div>
-      {postTitle}
+      <button onClick={() => {setCurrentPost(1)}}>1</button>
+      <button onClick={() => {setCurrentPost(2)}}>2</button>
+      <button onClick={() => {
+        setCurrentPost(3)
+      }}>3</button>
+      {JSON.stringify(finalData)}
     </div>
   )
 }
-
 export default App
 // //pretty basic custom hooks example
 // function useCounter(){
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -16,3 +16,27 @@ export function usePostTitle(){
 
         return post.title;
 }
+
+//what is the useFetch supposed to do: it will take the url as an input and it will return the state variable and returns some data, which can be a post or title , based on the url we are passing
+
+export function useFetch(url) {
+    const [loading, setLoading] = useState(true)
+    const [finalData , setFinalData] = useState({})
+
+    async function getDetails(){
+        setLoading(true);
+        const response = await fetch(url);
+        const json = await response.json();
+        setFinalData(json) ;
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        getDetails()
+    }, [url])
+
+    return {
+        finalData ,
+        loading
+    }
+}
